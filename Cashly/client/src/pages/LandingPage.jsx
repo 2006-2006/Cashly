5b4b936f98cb399a364afd33b64cea6bdcf95574
2006-2006 <yvijayakumar2006@gmail.com>
2025-12-31 18:17:02 +0530

    Fix: add .gitignore and deployment configs

--- Cashly/client/src/pages/LandingPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SaaSTemplate from '../components/ui/saa-s-template';
-
-const LandingPage = () => {
-    return <SaaSTemplate />;
-};
-
-export default LandingPage;
--- Cashly/server/routes/settingsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { clearAllUserData } = require('../controllers/settingsController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.delete('/clear-data', protect, clearAllUserData);
-
-module.exports = router;
